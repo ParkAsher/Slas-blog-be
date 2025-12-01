aeefea0ba33bee473fa381528d243e3be1bfcd5b
ParkAsher <devasherpark@naver.com>
2025-12-01 23:42:53 +0900

    [feat]: CORS 설정

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    // CORS
+    app.enableCors({
+        origin: [],
+        credentials: true,
+    });
+
+    await app.listen(8000);
 }
 bootstrap();
