8fe925b2ad03245ca7b48f9c374098bfdc0e2dc7
ParkAsher <devasherpark@naver.com>
2026-01-06 15:48:53 +0900

    [feat]: 도메인 주소 추가

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ async function bootstrap() {
 
     // CORS
     app.enableCors({
-        origin: ['http://localhost:3000', 'https://slas-log-fe.vercel.app'],
+        origin: [
+            'http://localhost:3000',
+            'https://slas-log-fe.vercel.app',
+            'https://slas.kr',
+        ],
         credentials: true,
     });
 
